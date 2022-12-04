f25b27dace0a73c8e45c0f3d1f93691f7cf387c6
rakeshkhaarol <rakeshkharol457@gmail.com>
2022-12-04 20:27:30 +0530

    data type

---
+++ js/script.js
@@ -0,0 +1,17 @@
+
+
+
+//string value
+let y = name;//stetment
+
+//4 type in outpute
+
+console.log(typeof y)//1 outpute
+
+window.alert(typeof y)//2 outpute
+
+document.write(typeof y)
+
+document.querySelector(`.xyz`).innerHTML= typeof y
+
+document.querySelector('[data-a5="a5"]').innerHTML= "hello everyone"
